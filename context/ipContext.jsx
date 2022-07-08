c943ad9064e11e25e5a31b594b18a947f9278e04
richardebrain <richardebrain20@gmail.com>
2022-07-08 16:11:34 +0100

    trying to deploy another way

--- context/ipContext.jsx
+++ context/ipContext.jsx
@@ -8,15 +8,16 @@ export const IpWrapper = ({ children }) => {
 
   const getIp = async(newIp) => {
     try {
-     const res =await  fetch(
-        '/api/ipFetcher',{
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(!newIp ? ipData : newIp)
+    //  const res =await  fetch(
+    //     '/api/ipFetcher',{
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify(!newIp ? ipData : newIp)
 
-        })
+    //     })
+    const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_obaZnNbeu8SgbcpG7H9dz9cQFqdmC&ipAddress=${!newIp ? ipData : newIp}`)
       const data = await res.json();
       setIpData(data);
       
